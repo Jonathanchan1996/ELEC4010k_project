9c22e6caf52c8ba2e4daef7ff91947909f88f5e4
Yan <38583507+SFiend@users.noreply.github.com>
2019-12-04 02:20:02 +0800

    Update circle_hough.py

--- src/ball_track/src/circle_hough.py
+++ src/ball_track/src/circle_hough.py
@@ -13,8 +13,21 @@ def colorFiltering(img): #yellow filter
     (thres, bw) = cv2.threshold(cv2.cvtColor(filtered, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
     return bw
 
+img = cv2.imread('circle.png')
+img = colorFiltering(img)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-cv2_img = cv2.imread('circle.png')
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,2,50,param1=50,param2=30,minRadius=0,maxRadius=100)
+if circles is None:
+    print("No circle is detected")
+    
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-cv2.imshow('detected circles',colorFiltering(cv2_img))
+cv2.imshow('detected circles',cimg)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
